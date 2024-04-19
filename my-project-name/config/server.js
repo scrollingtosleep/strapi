e8c16fc9511207bdc8b4c7eafb8d8f1ba97482cd
scrollingtosleep <157798334+scrollingtosleep@users.noreply.github.com>
2024-04-19 22:21:38 +0000

    karde

---
+++ my-project-name/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1338),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '64ca0c19316f639c116988be91341e00'),
+    },
+  },
+});
---
+++ my-project-name/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8d5b1339-3962-4bf4-901b-7c3116f01029'
+};
